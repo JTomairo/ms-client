4bafef72552115c3de1a54cfd8c986ad3950e69c
Juan Tomairo <jtomairo@everis.com>
2021-10-19 16:44:49 -0500

    feat: Add spring initializr

---
+++ src/main/java/com/banca/proyectoBancaSemana1/ProyectoBancaSemana1Application.java
@@ -0,0 +1,13 @@
+package com.banca.proyectoBancaSemana1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBancaSemana1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBancaSemana1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/banca/proyectoBancaSemana1/ProyectoBancaSemana1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banca.proyectoBancaSemana1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBancaSemana1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
