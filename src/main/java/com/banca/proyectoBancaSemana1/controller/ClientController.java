96a5984c064b190a3ee7bcef44b644de898d8ecc
Juan Tomairo <jtomairo@everis.com>
2021-10-24 12:17:16 -0500

    feat: Add log

--- src/main/java/com/banca/proyectoBancaSemana1/controller/ClientController.java
+++ src/main/java/com/banca/proyectoBancaSemana1/controller/ClientController.java
@@ -21,30 +21,35 @@ public class ClientController {
     @ApiOperation(value = "Get list of clients", response = String.class)
     @GetMapping("/listClients")
     public Flux<Client> listClients() {
+        log.info("Get list of clients");
         return clientService.findAll();
     }
 
     @ApiOperation(value = "Get a specific client", response = String.class)
     @GetMapping("/getClient/{id}")
     public Mono<Client> getClient(@PathVariable("id") String id) {
+        log.info("Get a specific client");
         return clientService.findById(id);
     }
 
     @ApiOperation(value = "Create client", response = String.class)
     @PostMapping("/addClient")
     public void createClient(@RequestBody Client client) {
+        log.info("Create client");
         clientService.save(client).subscribe();
     }
 
     @ApiOperation(value = "Update client", response = String.class)
     @PutMapping("/updateClient")
     public Mono<Client> updateClient(@RequestBody Client client) {
+        log.info("Update client");
         return clientService.update(client);
     }
 
     @ApiOperation(value = "Delete a specific client", response = String.class)
     @DeleteMapping("/deleteClient/{id}")
     public Mono<Void> deleteClient(@PathVariable("id") String id) {
+        log.info("Delete a specific client");
         return clientService.deleteById(id);
     }
 
