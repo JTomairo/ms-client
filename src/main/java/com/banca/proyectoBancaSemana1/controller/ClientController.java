cf1dcc4f3b879603a6186bb3ad3b2251e174b204
Juan Tomairo <jtomairo@everis.com>
2021-10-24 11:53:34 -0500

    feat: Add Swagger

--- src/main/java/com/banca/proyectoBancaSemana1/controller/ClientController.java
+++ src/main/java/com/banca/proyectoBancaSemana1/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.banca.proyectoBancaSemana1.controller;
 
 import com.banca.proyectoBancaSemana1.model.Client;
 import com.banca.proyectoBancaSemana1.service.impl.ClientServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,26 +12,31 @@ import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
+@Api(tags = {"REST APIs related to Client Entity"})
 public class ClientController {
 
     @Autowired
     private ClientServiceImpl clientService;
 
+    @ApiOperation(value = "Get list of clients", response = String.class)
     @GetMapping("/listClients")
     public Flux<Client> listClients() {
         return clientService.findAll();
     }
 
+    @ApiOperation(value = "Create client", response = String.class)
     @PostMapping("/addClient")
     public void createClient(@RequestBody Client client) {
         clientService.save(client).subscribe();
     }
 
+    @ApiOperation(value = "Update client", response = String.class)
     @PutMapping("/updateClient")
     public Mono<Client> updateClient(@RequestBody Client client) {
         return clientService.update(client);
     }
 
+    @ApiOperation(value = "Delete a specific client", response = String.class)
     @DeleteMapping("/deleteClient/{id}")
     public Mono<Void> deleteClient(@PathVariable("id") String id) {
         return clientService.deleteById(id);
